45537ca7b32310dcb6035b3fdc8291ce83d7649e
Khushwant Singh Rathore <rathore11khushwant@gmail.com>
2026-01-06 00:45:53 +0100

    Use csv source type for Feature Store

--- src/features/deploy_feature_store.py
+++ src/features/deploy_feature_store.py
@@ -75,7 +75,7 @@ def deploy_feature_set():
     cloud_url = get_cloud_path(config, "data/processed/features_train.csv")
     print(f"Resolved Cloud URL: {cloud_url}")
     
-    feature_spec['source']['type'] = 'uri_file'
+    feature_spec['source']['type'] = 'csv'
     feature_spec['source']['path'] = cloud_url
     
     
